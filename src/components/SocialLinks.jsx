46335948ef438b298332413502afa3bc77fd0fe2
Sameena Zubair <sameenazubair567@gmail.com>
2024-11-25 16:44:24 +0530

    resume link added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -44,6 +44,7 @@ const SocialLinks = () => {
         Resume <BsFillPersonLinesFill size={30}/>
         </>
     ),
+    href: 'https://drive.google.com/file/d/1a4SkVyBWdLv57A-ewhIdyNqeLuZcWSY8/view?usp=drivesdk'
  
  },
 
